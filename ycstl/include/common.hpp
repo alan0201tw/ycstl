8bd6a73de74e3dbb49f3849877fc2736e57633eb
DESKTOP-6J14KSJ\AlanShih <alan0201tw@gmail.com>
2024-06-10 00:24:15 +0800

    Rename yccommon to YcCommon

--- ycstl/include/common.hpp
+++ ycstl/include/common.hpp
@@ -3,7 +3,7 @@
 namespace ycstl {
 
 template <typename T>
-class yccommon {
+class YcCommon {
     static constexpr bool isSorted() { return T::isSorted(); }
     static constexpr bool isUnique() { return T::isUnique(); }
 };
