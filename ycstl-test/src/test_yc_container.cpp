fe644fb8abd67519aa1fe052d8114e268c4bc1f4
DESKTOP-6J14KSJ\AlanShih <alan0201tw@gmail.com>
2024-06-20 20:56:01 +0800

    Add support to more sorted ranges methods

--- ycstl-test/src/test_yc_container.cpp
+++ ycstl-test/src/test_yc_container.cpp
@@ -5,6 +5,25 @@
 #include "test_util.hpp"
 #include "ycstl.hpp"
 
+TEST(TestYcContainer, getSetUnion) {
+    auto testVector0 = ycstl::YcVector<int> {1, 2, 3, 4, 5, 6, 7};
+    EXPECT_FALSE(testVector0.isSorted());
+    auto testVector1 = ycstl::YcVector<int> {5, 6, 7, 8, 9, 10, 21, 22};
+    EXPECT_FALSE(testVector1.isSorted());
+
+    using OutputContainer = std::vector<int>;
+    EXPECT_THROW_WITH_INFO(testVector0.getSetUnion<OutputContainer>(testVector1), ycstl::exception::YcException,
+                           "Current container is not sorted");
+    testVector0.sort();
+    EXPECT_THROW_WITH_INFO(testVector0.getSetUnion<OutputContainer>(testVector1), ycstl::exception::YcException,
+                           "Rhs container is not sorted");
+    testVector1.sort();
+
+    const auto setUnion = testVector0.getSetUnion<OutputContainer>(testVector1);
+    const auto expectedAnswer = OutputContainer {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22};
+    EXPECT_EQ(setUnion, expectedAnswer);
+}
+
 TEST(TestYcContainer, getSetIntersection) {
     auto testVector0 = ycstl::YcVector<int> {1, 2, 3, 4, 5, 6, 7};
     EXPECT_FALSE(testVector0.isSorted());
@@ -22,4 +41,42 @@ TEST(TestYcContainer, getSetIntersection) {
     const auto setIntersection = testVector0.getSetIntersection<OutputContainer>(testVector1);
     const auto expectedAnswer = OutputContainer {5, 6, 7};
     EXPECT_EQ(setIntersection, expectedAnswer);
+}
+
+TEST(TestYcContainer, getSetDifference) {
+    auto testVector0 = ycstl::YcVector<int> {1, 2, 3, 4, 5, 6, 7};
+    EXPECT_FALSE(testVector0.isSorted());
+    auto testVector1 = ycstl::YcVector<int> {5, 6, 7, 8, 9, 10, 21, 22};
+    EXPECT_FALSE(testVector1.isSorted());
+
+    using OutputContainer = std::vector<int>;
+    EXPECT_THROW_WITH_INFO(testVector0.getSetDifference<OutputContainer>(testVector1), ycstl::exception::YcException,
+                           "Current container is not sorted");
+    testVector0.sort();
+    EXPECT_THROW_WITH_INFO(testVector0.getSetDifference<OutputContainer>(testVector1), ycstl::exception::YcException,
+                           "Rhs container is not sorted");
+    testVector1.sort();
+
+    const auto setDifference = testVector0.getSetDifference<OutputContainer>(testVector1);
+    const auto expectedAnswer = OutputContainer {1, 2, 3, 4};
+    EXPECT_EQ(setDifference, expectedAnswer);
+}
+
+TEST(TestYcContainer, getSetSymmetricDifference) {
+    auto testVector0 = ycstl::YcVector<int> {1, 2, 3, 4, 5, 6, 7};
+    EXPECT_FALSE(testVector0.isSorted());
+    auto testVector1 = ycstl::YcVector<int> {5, 6, 7, 8, 9, 10, 21, 22};
+    EXPECT_FALSE(testVector1.isSorted());
+
+    using OutputContainer = std::vector<int>;
+    EXPECT_THROW_WITH_INFO(testVector0.getSetSymmetricDifference<OutputContainer>(testVector1),
+                           ycstl::exception::YcException, "Current container is not sorted");
+    testVector0.sort();
+    EXPECT_THROW_WITH_INFO(testVector0.getSetSymmetricDifference<OutputContainer>(testVector1),
+                           ycstl::exception::YcException, "Rhs container is not sorted");
+    testVector1.sort();
+
+    const auto setSymmetricDifference = testVector0.getSetSymmetricDifference<OutputContainer>(testVector1);
+    const auto expectedAnswer = OutputContainer {1, 2, 3, 4, 8, 9, 10, 21, 22};
+    EXPECT_EQ(setSymmetricDifference, expectedAnswer);
 }
--- ycstl/include/common.hpp
+++ ycstl/include/common.hpp
@@ -6,6 +6,23 @@ namespace ycstl {
 
 template <typename T>
 class IYcContainer {
+private:
+#define GEN_RANGE_METHOD(yc_method, std_method)                                                         \
+    template <typename OutputContainer>                                                                 \
+    OutputContainer yc_method(const auto& rhs) const {                                                  \
+        if (false == isSorted()) {                                                                      \
+            exception::YcException::ThrowWithMessage("Current container is not sorted");                \
+        }                                                                                               \
+        if (false == rhs.isSorted()) {                                                                  \
+            exception::YcException::ThrowWithMessage("Rhs container is not sorted");                    \
+        }                                                                                               \
+        auto outputContainer = OutputContainer {};                                                      \
+        const auto inserter = std::back_inserter(outputContainer);                                      \
+        const auto castedThis = static_cast<const T*>(this);                                            \
+        (void)std_method(castedThis->cbegin(), castedThis->cend(), rhs.cbegin(), rhs.cend(), inserter); \
+        return outputContainer;                                                                         \
+    }
+
 public:
     bool isSorted() const { return static_cast<const T*>(this)->isSorted(); }
     bool isUnique() const { return static_cast<const T*>(this)->isUnique(); }
@@ -20,20 +37,12 @@ public:
         return true;
     }
 
-    template <typename OutputContainer>
-    OutputContainer getSetIntersection(const auto& rhs) const {
-        if (false == isSorted()) {
-            exception::YcException::ThrowWithMessage("Current container is not sorted");
-        }
-        if (false == rhs.isSorted()) {
-            exception::YcException::ThrowWithMessage("Rhs container is not sorted");
-        }
-        auto outputContainer = OutputContainer {};
-        const auto castedThis = static_cast<const T*>(this);
-        (void)std::set_intersection(castedThis->cbegin(), castedThis->cend(), rhs.cbegin(), rhs.cend(),
-                                    outputContainer.begin());
-        return outputContainer;
-    }
+    GEN_RANGE_METHOD(getSetUnion, std::set_union);
+    GEN_RANGE_METHOD(getSetIntersection, std::set_intersection);
+    GEN_RANGE_METHOD(getSetDifference, std::set_difference);
+    GEN_RANGE_METHOD(getSetSymmetricDifference, std::set_symmetric_difference);
+
+#undef GEN_RANGE_METHOD
 };
 
 }  // namespace ycstl
